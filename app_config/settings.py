0e1f573e616ddc1687f7f320eccd3e9473821202
Avi <avico78@gmail.com>
2023-01-18 12:54:56 +0200

    setting class (singelton)

---
+++ app_config/settings.py
@@ -0,0 +1,42 @@
+import yaml
+from functools import reduce
+import operator
+from pathlib import Path
+
+
+class SingletonMeta(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Settings(metaclass=SingletonMeta):
+    path_configuration = Path('app_config') / 'config.yaml'
+
+    def __init__(self):
+        with open(Settings.path_configuration, "r") as stream:
+            try:
+                self.settings = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+    def get(self, element):
+        return reduce(operator.getitem, element.split('.'), self.settings)
+
+
+# setting = Settings()
+
+# if __name__ == "__main__":
+#     # The client code.
+#
+#     s1 = Settings()
+#     s2 = Settings()
+#     print(s1.get('databases.mongo.ENGINE'))
+#     if id(s1) == id(s2):
+#         print("Singleton works, both variables contain the same instance.")
+#     else:
+#         print("Singleton failed, variables contain different instances.")
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-def print_hi(name):
+from app_config.settings import Settings
+def main(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    settings = Settings()
+    print(settings.get('databases.mongo.ENGINE'))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
